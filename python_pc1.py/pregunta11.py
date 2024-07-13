26609e1cccc0d1e4f4228ad5f914cade0404b515
fakerenato <renato272000@gmail.com>
2024-07-13 18:32:06 +0000

    resolucion practica 1 renato layme

--- python_pc1.py/pregunta11.py
+++ python_pc1.py/pregunta11.py
@@ -1,1 +1,10 @@
-list= [1, 1, 2, 3, 4, 4, 5, 1]
+def eliminar_duplicados(lista):
+   
+    lista_sin_duplicados = list(set(lista))
+    return lista_sin_duplicados
+
+lista_original = [1, 1, 2, 3, 4, 4, 5, 1]
+
+lista_sin_duplicados = eliminar_duplicados(lista_original)
+print("Lista sin duplicados:", lista_sin_duplicados)
+
